87621520b5fe9fe14485dd7c2a5f94cfa52f0beb
Tionne <whelan.tionne@gmail.com>
2022-06-07 07:41:22 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#include "putchar_"
+#include "holberton.h"
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *   * main - Entry point
+ *     *
+ *       * Return: Always 0
+ *         */
 int main(void)
 {
-		char *sh = "putchar_";
+		_putchar('H');
+			_putchar('o');
+				_putchar('l');
+					_putchar('b');
+						_putchar('e');
+							_putchar('r');
+								_putchar('t');
+									_putchar('o');
+										_putchar('n');
+											_putchar('\n');
 
-			while (*sh)
-					{
-								_putchar(*sh);
-										sh++;
-											}
-				_putchar('\n');
-
-					return (0);
+												return (0);
 }
