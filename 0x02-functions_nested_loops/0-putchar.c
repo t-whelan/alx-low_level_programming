34e6a561a61f5aef01ec3776c1f91af8ed5cbbae
Tionne <whelan.tionne@gmail.com>
2022-06-07 13:25:44 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,25 +3,11 @@
  *
  */
 
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+*/ int main(void)
 	_putchar('\n');
 
 	return (0);
-}
-pwd
+} 
