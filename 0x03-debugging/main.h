3fb5f0875e2aafc5313a0c749cc77e583a011530
Tionne <whelan.tionne@gmail.com>
2022-06-08 03:44:51 -0700

    0 Task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifindef MAIN_H
+#define Main_H
+ #include <studio.h>
+void poistive_or_negative(int i);
+int largest_number(int a, int b, int c,);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif
