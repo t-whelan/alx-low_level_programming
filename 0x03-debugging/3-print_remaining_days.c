1f3c24896e815734efb0057404d602f2dbe9956c
Tionne <whelan.tionne@gmail.com>
2022-06-09 04:53:00 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
   * print_remaining_days - takes a date and prints how many days are
@@ -32,5 +32,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
+
 	}
-}
+}	
