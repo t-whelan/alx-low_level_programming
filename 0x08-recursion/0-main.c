176fc46bfa3a73790bfbd0904fdb18a83d70b0be
Tionne <whelan.tionne@gmail.com>
2022-06-16 05:38:24 -0700

    0-main.c

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for Alx School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
